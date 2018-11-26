70196771c5885008edf364d01186bf1e5b66a25b
Curtis Taylor Thomas III <thomas.curtis09@gmail.com>
2018-11-25 21:54:52 -0500

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,54 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+For this first project we will be using Workspaces.
+
+NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+
+"""
+
+import random
+
+
+def start_game():
+    """Psuedo-code Hints
+
+    When the program starts, we want to:
+    ------------------------------------
+    1. Display an intro/welcome message to the player.
+    2. Store a random number as the answer/solution.
+    3. Continuously prompt the player for a guess.
+      a. If the guess greater than the solution, display to the player "It's lower".
+      b. If the guess is less than the solution, display to the player "It's higher".
+
+    4. Once the guess is correct, stop looping, inform the user they "Got it"
+         and show how many attempts it took them to get the correct number.
+    5. Let the player know the game is ending, or something that indicates the game is over.
+
+    ( You can add more features/enhancements if you'd like to. )
+    """
+    # write your code inside this function.
+    attempts = 0
+    guess_number = 0
+    random_number = random.randint(0, 10)
+
+    print("********* Welcome to Guess That Number! *********")
+
+    while guess_number != random_number:
+        guess_number = int(input("Pick a number between 1 and 10:"))
+
+        attempts = attempts + 1
+
+        if(random_number < guess_number):
+            print("It's lower")
+        elif(random_number > guess_number):
+            print("It's higher")
+        else:
+            print("You Guessed it! It took {} attempts".format(attempts))
+
+
+    if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.
+    start_game()
